8c2974c4fc26963716a082bce8cea5792434cfc5
William Cheong <williamcheong06@outlook.com>
2022-07-06 01:01:17 +0800

    latest update p2q5 on going 10%....

---
+++ PCD1/p2q5.c
@@ -0,0 +1,14 @@
+/*
+Prorame Filename: P2Q5
+Purpose :	Calculates the area and circumference of a circle
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#pragma warning (disable: 4996)
+
+void main(void)
+{
+		//variable declaration
+
+}
