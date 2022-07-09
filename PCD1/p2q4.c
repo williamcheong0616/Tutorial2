ea7f87739abfb38b8a82cc6080fb86155e48b76b
William Cheong <williamcheong06@outlook.com>
2022-07-09 13:59:45 +0800

    p2q5 latest update -error

--- PCD1/p2q4.c
+++ PCD1/p2q4.c
@@ -1,42 +1,42 @@
-/*
-Program Filename : P2Q4
-Purpose			 : 
-*/
-#include<stdio.h>
-#include<stdlib.h>
-#pragma warning (disable: 4996)
-
-void main(void)
-{
-	//Variable declaration
-	char firstName[11], lastName[21];
-	int	heightCM;
-	int	ageInYears, ageInDays;
-	double heightIN;
-
-	//input
-
-	printf("Enter your first name      :");
-	gets(&firstName);
-
-	printf("Enter your last name       :");
-	gets(&lastName);
-
-	printf("Enter your height in \"cm\" : ");
-	scanf("%d", &heightCM);
-
-	printf("Enter your age in \"years\" :");
-	scanf("%d", &ageInYears);
-
-	//process
-	heightIN = heightCM / 2.54;
-	ageInDays = ageInYears * 365;
-
-	//Output
-	printf("\n %s %s \n", firstName, lastName);
-	printf("your height is \t %f inches.\n", heightIN);
-	printf("Your age is %d days!!!\n");
-
-	system("pause");
-
-}
+///*
+//Program Filename : P2Q4
+//Purpose			 :  Calculate the height in inches & calculate days of age
+//*/
+//#include<stdio.h>
+//#include<stdlib.h>
+//#pragma warning (disable: 4996)
+//
+//void main(void)
+//{
+//	//Variable declaration
+//	char firstName[11], lastName[21];
+//	int	heightCM;
+//	int	ageInYears, ageInDays;
+//	double heightIN;
+//
+//	//input
+//
+//	printf("Enter your first name      :");
+//	gets(&firstName);
+//
+//	printf("Enter your last name       :");
+//	gets(&lastName);
+//
+//	printf("Enter your height in \"cm\" : ");
+//	scanf("%d", &heightCM);
+//
+//	printf("Enter your age in \"years\" :");
+//	scanf("%d", &ageInYears);
+//
+//	//process
+//	heightIN = heightCM / 2.54;
+//	ageInDays = ageInYears * 365;
+//
+//	//Output
+//	printf("\n %s %s \n", firstName, lastName);
+//	printf("your height is \t %f inches.\n", heightIN);
+//	printf("Your age is %d days!!!\n");
+//
+//	system("pause");
+//
+//}
--- PCD1/p2q5.c
+++ PCD1/p2q5.c
@@ -6,9 +6,28 @@ Purpose :	Calculates the area and circumference of a circle
 #include<stdio.h>
 #include<stdlib.h>
 #pragma warning (disable: 4996)
+#define PI 3.14159
+// PI (ie ¦Ð) has a constant (fixed) value. 
 
 void main(void)
 {
-		//variable declaration
+	//variable declaration
+	double area;	// to store the values
+	int circumference; // to store the values
+	int radius;	// to store the values
 
+	//input from the user
+	printf("Enter the radius of the circle :"); // 
+	scanf("%if", &radius);
+
+		PI = 3.141593;
+		area = PI * radius * radius; // calculate area of the circle
+		circumference = 2 * PI * radius; //calculate circumference of the circle
+
+	 //output of the calculation
+
+	printf("Area = %.2f\n", area); //display area value
+	printf("Circumference = %.2f\n", circumference); //display circumference value
+
+	system("pause");
 }
