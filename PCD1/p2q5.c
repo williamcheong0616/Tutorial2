1d0528d82773115360d276fcc7aa3c43187d1a9e
William Cheong <williamcheong06@outlook.com>
2022-07-18 06:32:58 +0800

    Latest update on p2q5

--- PCD1/p2q5.c
+++ PCD1/p2q5.c
@@ -9,23 +9,21 @@ Purpose :	Calculates the area and circumference of a circle
 #define PI 3.14159
 // PI (ie ¦Ð) has a constant (fixed) value. 
 
-void main(void)
+void main()
 {
 	//variable declaration
-	double area;	// to store the values
-	int circumference; // to store the values
-	int radius;	// to store the values
+	double area, circumference, radius;
 
 	//input from the user
 	printf("Enter the radius of the circle :"); // 
-	scanf("%if", &radius);
+	scanf("%lf", &radius);
+	
+		
+	 //process the calculation
+	area = PI * radius * radius; // calculate area of the circle
+	circumference = 2 * PI * radius; //calculate circumference of the circle
 
-		PI = 3.141593;
-		area = PI * radius * radius; // calculate area of the circle
-		circumference = 2 * PI * radius; //calculate circumference of the circle
-
-	 //output of the calculation
-
+	//outout of the calcualtion
 	printf("Area = %.2f\n", area); //display area value
 	printf("Circumference = %.2f\n", circumference); //display circumference value
 
